3158c7adb8ce7d00154539a645dd063c2f3e88c9
Shreya <shreya.roy.blr@gmail.com>
2021-01-08 14:45:19 +0530

    updated app

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html");
