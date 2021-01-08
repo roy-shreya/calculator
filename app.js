f9fcf64ecf89b2a2d9cc7b871e13207517dc2a56
Shreya <shreya.roy.blr@gmail.com>
2021-01-08 13:28:41 +0530

    added procfile

--- app.js
+++ app.js
@@ -26,6 +26,6 @@ app.post("/bmicalculator", function(req, res){
     res.send("Your BMI is: "+bmi);
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server running on port 3000")
 });
